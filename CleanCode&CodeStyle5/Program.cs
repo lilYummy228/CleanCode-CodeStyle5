30fdb536306ccc0d498103f36bc9407ad3c5eab8
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2024-09-30 21:28:22 +0500

    Добавьте файлы проекта.

---
+++ CleanCode&CodeStyle5/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+public class Player { }
+
+public class Gun { }
+
+public class TargetFollower { }
+
+public class Unit { }
+
+class UnitBase
+{
+    public IReadOnlyCollection<Unit> Units { get; private set; }
+}
